c874b93d26c639628997f0cf37a00835783c85e4
Al McElrath <hello@yrns.org>
2026-01-18 07:27:41 -0800

    entity color

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 use bevy_ecs::prelude::*;
 // use bevy_transform::components::Transform;
 
+/// Options for [`DebugEntityHierarchy`].
+#[derive(Resource)]
+pub struct Options {
+    pub color: bool,
+}
+
+impl Options {
+    /// Default [`Options`].
+    const DEFAULT: &'static Self = &Self { color: true };
+}
+
 /// Displays an entity hierarchy with component names.
 #[must_use = "must be displayed"]
 pub struct DebugEntityHierarchy<'w> {
@@ -24,6 +35,11 @@ pub fn debug_entity_hierarchy(entity: Entity, world: &World) -> DebugEntityHiera
 
 impl std::fmt::Display for DebugEntityHierarchy<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let options = self
+            .world
+            .get_resource::<Options>()
+            .unwrap_or(Options::DEFAULT);
+
         let mut entities = vec![(self.entity, String::new(), true)];
 
         while let Some((entity, mut prefix, last_child)) = entities.pop() {
@@ -34,9 +50,21 @@ impl std::fmt::Display for DebugEntityHierarchy<'_> {
 
             write!(f, "{}", prefix)?;
 
+            let style = if options.color {
+                let style = color::color_from_entity(entity);
+                write!(f, "{}", style.prefix())?;
+                style
+            } else {
+                Default::default()
+            };
+
             match self.world.get::<Name>(entity) {
                 Some(name) => write!(f, "\"{name}\" ({entity})")?,
                 None => write!(f, "{entity}")?,
+            }
+
+            if options.color {
+                write!(f, "{}", style.suffix())?;
             }
 
             let mut components = self
@@ -89,6 +117,20 @@ impl std::fmt::Display for DebugEntityHierarchy<'_> {
     }
 }
 
+mod color {
+    use bevy_color::{ColorToPacked, LinearRgba, Oklcha};
+    use nu_ansi_term::Color;
+
+    use super::*;
+
+    // Match gizmo color.
+    pub(crate) fn color_from_entity(entity: Entity) -> Color {
+        let [r, g, b] =
+            LinearRgba::from(Oklcha::sequential_dispersed(entity.index())).to_u8_array_no_alpha();
+        Color::Rgb(r, g, b)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -96,6 +138,9 @@ mod tests {
     #[test]
     fn test_debug_entity_hierarchy() {
         let mut world = World::default();
+
+        world.insert_resource(Options { color: false });
+
         let root = world
             .spawn(Name::new("root"))
             .with_children(|p| {
